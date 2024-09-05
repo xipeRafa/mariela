4a2d2d2ec0d07626da8c6a0f9e7f8882235721d6
fer <fer@fers-iMac.local>
2024-09-05 14:44:38 -0700

    koko

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+          <h1> Mariela Rosario del Pilar Ramirez Robles de Cristo Jesus Rey y Redentor Nuestro </h1> 
+          <p>Aqui esta tu pagina, tego todo el dia libre hoy y mañana : )</p >
+    </>
+  )
+}
+
+export default App
