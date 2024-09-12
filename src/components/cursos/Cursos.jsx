d42a74949d685bf56be0365ce8f0b648fcf00df9
fer <fer@fers-iMac.local>
2024-09-11 18:05:05 -0700

    lplp

--- src/components/cursos/Cursos.jsx
+++ src/components/cursos/Cursos.jsx
@@ -2,14 +2,19 @@
 import './cursos.css'
 
 
-import img111 from "../../imgs/1.png";
+import c1 from "../../imgs/curso1.jpeg";
+import c2 from "../../imgs/curso2.jpeg";
+
+
+
+
+
 
 export default function Cursos() {
 
 
-	  return (
-
-
-<>cursos</>
-	  	)
+	  return (<>
+	  			<img src={c2} className='curso' />
+	  			<img src={c1} className='curso'/>
+	  	</>)
 }
